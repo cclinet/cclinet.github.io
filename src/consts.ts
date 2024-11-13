1d5b9c9ffa7e459740b3d673a57f2580c4ca3f52
cclin <cclinet@outlook.com>
2024-11-13 11:16:47 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "CCLIN";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "探机之家";
+export const SITE_DESCRIPTION = "探机的自我修养";
