29be79a329d635935fd15892bb625c85a4c80e9e
cclin <cclinet@outlook.com>
2024-12-05 16:18:40 +0800

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,10 @@
 import pluginJs from "@eslint/js";
 import eslintPluginAstro from "eslint-plugin-astro";
-import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...eslintPluginAstro.configs['all'],
+  ...eslintPluginAstro.configs["all"],
 ];
