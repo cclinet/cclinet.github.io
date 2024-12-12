47680ce53a1889f45fa0e96b3b4e65aeab6db3c2
cclin <cclinet@outlook.com>
2024-12-12 10:47:10 +0800

    update

--- uno.config.ts
+++ uno.config.ts
@@ -4,16 +4,30 @@ import {
   presetIcons,
   presetTypography,
   presetUno,
+  presetWebFonts,
 } from "unocss";
 
 import { SocialLinks } from "./src/consts";
 
 export default defineConfig({
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["LXGW WenKai", "Noto Sans SC", "sans-serif"],
+      },
+    },
+  },
   presets: [
     presetAttributify(), // required when using attributify mode
     presetUno(), // required
     presetTypography({}),
     presetIcons(),
+    presetWebFonts({
+      provider: "fontshare",
+      fonts: {
+        sans: "LXGW WenKai",
+      },
+    }),
   ],
   safelist: [...SocialLinks.map((social) => `i-mdi-${social.name}`)],
 });
