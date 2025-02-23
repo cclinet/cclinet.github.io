66f927df9b74f5adb1f536edbb2b944b6d40e999
cclin <cclinet@outlook.com>
2025-02-23 23:11:42 +0800

    fix i18n and navigation

--- src/components/posts.ts
+++ src/components/posts.ts
@@ -3,7 +3,7 @@ import { getCollection } from "astro:content";
 const posts = (await getCollection("blog")).map((post) => {
   const [locale, ...title_components] = post.id.split("/");
   const id_components =
-    title_components.at(-1) == title_components.at(-2)
+    title_components.at(-1) === title_components.at(-2)
       ? title_components.slice(0, -1)
       : title_components;
   const id = id_components.join("/");
