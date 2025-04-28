597077be8a0b8fa119cc1b777302a621217cc8b1
cclin <cclinet@outlook.com>
2025-04-28 14:16:21 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 import tailwindcss from "@tailwindcss/vite";
 import expressiveCode from "astro-expressive-code";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 
@@ -37,7 +37,13 @@ export default defineConfig({
     defaultLocale: "zh",
   },
   experimental: {
-    svg: true,
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "LXGW WenKai",
+        cssVariable: "--font-lxgw",
+      },
+    ],
     responsiveImages: true,
     clientPrerender: true,
   },
