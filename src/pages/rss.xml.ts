38b90d8ac485b65e7f80c598dca51dca7b4c04a4
cclin <cclinet@outlook.com>
2025-06-20 15:22:39 +0800

    add menu

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,14 +4,14 @@ import type { APIContext } from "astro";
 import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 import posts from "../posts";
 
-const pub_posts = posts.filter((post) => !post.data.draft);
+const pubPosts = posts.filter((post) => !post.data.draft);
 
 export async function GET(context: APIContext) {
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site!,
-    items: pub_posts.map((post) => ({
+    items: pubPosts.map((post) => ({
       ...post.data,
       link: `/posts/${post.id}/`,
     })),
--- src/posts.ts
+++ src/posts.ts
@@ -1,8 +1,8 @@
 import { getCollection } from "astro:content";
 
 const posts = (await getCollection("blog")).map((post) => {
-  const [locale, ...id_components] = post.id.split("/");
-  const id = id_components.join("/");
+  const [locale, ...idComponents] = post.id.split("/");
+  const id = idComponents.join("/");
   return {
     ...post,
     locale: locale!,
@@ -10,8 +10,8 @@ const posts = (await getCollection("blog")).map((post) => {
   };
 });
 const mathCollection = (await getCollection("mathCollection")).map((post) => {
-  const [locale, category, ...id_components] = post.id.split("/");
-  const id = id_components.join("/");
+  const [locale, category, ...idComponents] = post.id.split("/");
+  const id = idComponents.join("/");
   return {
     ...post,
     locale: locale!,
